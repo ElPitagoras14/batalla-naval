ee637ecca53b3562be725d93717c9b811058125e
Jonathan Garcia <jonfrgar@espol.edu.ec>
2021-12-03 14:17:45 -0500

    Probably ended

--- principal.asm
+++ principal.asm
@@ -1,17 +1,25 @@
 .model small
 .stack 260h
 .data
-    msg db "Ingrese ENTER para continuar", 0ah, 0dh, 24h
+    msg db 0ah, 0dh, "Ingrese ENTER para continuar y asignar aleatoriamente los barcos", 0ah, 0dh, 24h
     msg_err_pos db " - No se pudo ubicar la nave, fuera de rango", 0ah, 0dh, 24h
     msg_ing db "Ingrese columna y fila (Ej. A1): ", 24h
     msg_err_disp db " - Posicion no disponible", 0ah, 0dh, 24h
     
+    msg_ganador db "Felicitaciones has ganado", 0ah, 0dh, 24h
+    msg_perdedor db "Lamentablemente no has hallado todos los barcos ):", 0ah, 0dh, 24h
+    msg_repetir db "Desea jugar nuevamente (Y/N): ", 24h
+    
+    si_impacto db " - Impacto realizado", 0ah, 0dh, 24h
+    no_impacto db " - Sin impacto", 0ah, 0dh, 24h
+    
     pos_ini dw 37
-    vida db 20
+    vida db 0
     
     porta_avion db 5
     crucero db 4
     submarino db 3
+    
     barco_total db 5+4+3
     
     imp_tablero db 0ah, 0dh, "El tablero:", 0ah, 0ah, 0dh
@@ -42,10 +50,22 @@
 singleton proc
     repetir:
         call principal
-        ;Imprimir mensaje si desea salir
-        ;Evaluar
-        ;Si entonces jmp repetir
-        ;Sino ret
+        mov ah, 09h         
+        lea dx, msg_repetir
+        int 21h
+        
+        mov ah, 01h         ;funcion para captura de dato LETRA
+        int 21h
+        cmp al, "Y"
+        je limpieza
+        jmp terminar_programa
+        
+    limpieza:
+        call limpiar_tablero
+        jmp repetir
+    
+    terminar_programa:
+        ret
 singleton endp
 
 principal proc
@@ -54,90 +74,126 @@ principal proc
     xor bx, bx
     xor cx, cx
     xor dx, dx
-    jmp imprimir
+    call imprimir
     
     get_pos:
-    mov ah, 09h         
-    lea dx, msg_ing
-    int 21h
+        mov ah, 09h         
+        lea dx, msg_ing
+        int 21h
+        
+        mov ah, 01h         ;funcion para captura de dato LETRA
+        int 21h
+        mov bh, al
+        
+        mov ah, 01h         ;funcion para captura de dato NUM
+        int 21h
+        mov bl, al   
+        jmp rango
     
-    mov ah, 01h         ;funcion para captura de dato LETRA
-    int 21h
-    mov bh, al
+    rango: 
+        cmp bh, 46h
+        jnle error_pos
+        cmp bh, 41h
+        jnge error_pos
+        cmp bl, 36h
+        jnle error_pos
+        cmp bl, 31h
+        jnge error_pos
+        jmp cmp_disponible
+        
+    cmp_disponible:
+        call get_pos_tab
+        mov bx, offset cmp_tab
+        xor ax, ax
+        mov al, byte ptr [bx + di]
+        
+        cmp ax, "0"
+        je error_disp
+        cmp ax, "1"
+        je error_disp
+        
+        mov bl, vida
+        add bl, 1
+        mov vida, bl
+        
+        cmp ax, "."
+        je no_barco
+        jmp si_barco
     
-    mov ah, 01h         ;funcion para captura de dato NUM
-    int 21h
-    mov bl, al   
-    jmp rango
+    error_disp:
+        mov ah, 09h         
+        lea dx, msg_err_disp
+        int 21h
+        jmp get_pos    
+        
+    error_pos:
+        mov ah, 09h         
+        lea dx, msg_err_pos
+        int 21h  
+        jmp get_pos
     
-rango: 
-    cmp bh, 46h
-    jnle error_pos
-    cmp bh, 41h
-    jnge error_pos
-    cmp bl, 36h
-    jnle error_pos
-    cmp bl, 31h
-    jnge error_pos
-    jmp cmp_disponible
+    no_barco:
+        mov ah, 09h         
+        lea dx, no_impacto
+        int 21h  
+        mov bx, offset tablero
+        mov byte ptr [bx + di], "0"
+        mov bx, offset cmp_tab
+        mov byte ptr [bx + di], "0"
+        mov cl, vida
+        cmp cl, 20
+        je perdedor
+        call imprimir
+        jmp get_pos
+        
+    si_barco:
+        mov ah, 09h         
+        lea dx, si_impacto
+        int 21h  
+        mov bx, offset tablero
+        mov byte ptr [bx + di], "1"
+        mov bx, offset cmp_tab
+        mov byte ptr [bx + di], "1"
+        jmp comprobar_estado:
+        
+    comprobar_estado:
+        xor cx, cx
+        mov cl, barco_total
+        sub cl, 1
+        mov barco_total, cl
+        cmp cl, 0
+        je ganador
+        mov cl, vida
+        cmp cl, 20
+        je perdedor
+        call imprimir
+        jmp get_pos
     
-cmp_disponible:
-    call get_pos_tab
-    mov bx, offset cmp_tab
-    xor ax, ax
-    mov al, byte ptr [bx + di]
+    ganador:
+        call imprimir
+        mov ah, 09h         
+        lea dx, msg_ganador
+        int 21h
+        jmp salir_principal
+        
+    perdedor:        
+        call imprimir
+        mov ah, 09h         
+        lea dx, msg_perdedor
+        int 21h
+        jmp salir_principal
+        
+    salir_principal:        
+        ret
     
-    cmp ax, "0"
-    je error_disp
-    cmp ax, "1"
-    je error_disp
-    
-    mov bl, vida
-    sub bl, 1
-    mov vida, bl
-    
-    cmp ax, "."
-    je no_barco
-    jmp si_barco
+principal endp
 
-error_disp:
-    mov ah, 09h         
-    lea dx, msg_err_disp
-    int 21h
-    jmp get_pos    
-    
-
-    
-error_pos:
-    mov ah, 09h         
-    lea dx, msg_err_pos
-    int 21h  
-    jmp get_pos
-
-no_barco:
-    mov bx, offset tablero
-    mov byte ptr [bx + di], 30h
-    mov bx, offset cmp_tab
-    mov byte ptr [bx + di], 30h
-    jmp imprimir
-    
-si_barco:
-    mov bx, offset tablero
-    mov byte ptr [bx + di], 31h
-    mov bx, offset cmp_tab
-    mov byte ptr [bx + di], 31h
-    jmp imprimir
-    
-
-imprimir:
+imprimir proc
     mov ah, 09h         
     lea dx, imp_tablero
     int 21h
-    jmp get_pos
-    
     ret
-    
-principal endp
+imprimir endp
 
 get_pos_tab proc
     push ax
@@ -165,6 +221,8 @@ limpiar_tablero proc
     xor bx, bx
     xor cx, cx
     xor dx, dx
+    mov vida, 0
+    mov barco_total, 5+3+4
     mov ax, 1 ;fila
     mov di, pos_ini
     push ax
@@ -174,21 +232,20 @@ limpiar_tablero proc
     mov ax, 0 ;recorre columna
     
     limpiar:
-        add al, 1
         mov dh, al
         mov dl, 4
         mul dl
         push di
         add di, ax
         mov bx, offset tablero
-        mov cl, byte ptr[bx + di]
         mov byte ptr[bx + di], "."
         mov bx, offset cmp_tab
         mov byte ptr[bx + di], "."
         pop di
         xor ax, ax
         mov al, dh
-        cmp ax, 5
+        add al, 1
+        cmp ax, 6
         je aumentar_fila
         jmp limpiar
     
@@ -196,7 +253,7 @@ limpiar_tablero proc
         mov di, pos_ini
         pop ax
         add ax, 1
-        cmp ax, 5
+        cmp ax, 7
         je salir_limpiar
         push ax
         mov dl, 31
